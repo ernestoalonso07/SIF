4b0bee39a39d8fea5be8caf4ba20e253a151febd
ernestoalonso07 <ernesto.garcia.dgsif@gmail.com>
2024-06-06 14:25:58 -0700

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -11,28 +11,24 @@ pipeline {
         stage('Construir') {
             steps {
                 sh 'echo "Construyendo proyecto..."'
-                // Agrega aquí cualquier comando de construcción que necesites ejecutar
             }
         }
         
         stage('Pruebas Unitarias') {
             steps {
                 sh 'echo "Ejecutando pruebas unitarias..."'
-                // Agrega aquí comandos para ejecutar tus pruebas unitarias
             }
         }
         
         stage('Pruebas de Integración') {
             steps {
                 sh 'echo "Ejecutando pruebas de integración..."'
-                // Agrega aquí comandos para ejecutar tus pruebas de integración
             }
         }
         
         stage('Desplegar') {
             steps {
                 sh 'echo "Desplegando proyecto..."'
-                // Agrega aquí cualquier comando de despliegue que necesites ejecutar
             }
         }
     }
